41941181344fe4a7cc9f8f774bf03f59d13b9e30
jasonkx <technik@jabommi.de>
2013-04-10 11:27:03 +0300

    Create jkx_oxbasketitem.php

---
+++ modules/backend_order/core/jkx_oxbasketitem.php
@@ -0,0 +1,29 @@
+<?php
+class jkx_oxBasketitem extends jkx_oxBasketitem_parent{
+
+
+     /**
+     * Initializes basket item from oxorderarticle object
+     *  - oxbasketitem::_setFromOrderArticle() - assigns $oOrderArticle parameter
+     *  to oxBasketItem::_oArticle. Thus oxOrderArticle is used as oxArticle (calls
+     *  standard methods implemented by oxIArticle interface);
+     *  - oxbasketitem::setAmount();
+     *  - oxbasketitem::_setSelectList();
+     *  - oxbasketitem::setPersParams().
+     *  - oxbasketitem::setPrice().
+     *
+     * @param oxorderarticle $oOrderArticle order article to load info from
+     *
+     * @return null
+     */
+    public function initFromOrderArticle( $oOrderArticle)
+    {
+        parent::initFromOrderArticle( $oOrderArticle );
+
+        $this->setPrice($oOrderArticle->getPrice());
+    }
+
+
+
+
+}
