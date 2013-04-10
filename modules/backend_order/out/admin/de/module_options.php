b5e653152af41893d8f9de466aecb179d577c7f1
jasonkx <technik@jabommi.de>
2013-04-10 11:24:29 +0300

    Create module_options.php

---
+++ modules/backend_order/out/admin/de/module_options.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$sLangName  = "Deutsch";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+
+'charset'                                   => 'UTF-8',
+
+'SHOP_MODULE_GROUP_order'       => 'Bestellung',
+
+'SHOP_MODULE_jbRecalculateOrderArticlePrice'          => 'Aktuellen Artikelpreis statt Bestellartikelpreis bei Aktualisierung der Bestellung im Backend verwenden?',
+);
