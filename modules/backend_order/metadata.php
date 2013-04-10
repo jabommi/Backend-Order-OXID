29951f73c0e057247f3a7b55c212890559a25e31
jasonkx <technik@jabommi.de>
2013-04-10 11:20:51 +0300

    Update metadata.php

--- modules/backend_order/metadata.php
+++ modules/backend_order/metadata.php
@@ -1,1 +1,23 @@
-h
+<?php
+
+/**
+* Module information
+*/
+$aModule = array(
+    'id' => 'backend_order',
+    'title' => 'Backend Order',
+    'description' => 'Deactivate Recalculating price, discount, shipping, payment in Backend Order Managment.',
+    'version' => '0.5',
+    'author' => 'Jason KX',
+
+
+    'extend' => array(
+        'oxbasket' => 'backend_order/core/jkx_oxbasket',
+        'oxbasketitem' => 'backend_order/core/jkx_oxbasketitem'
+    ),
+
+    'settings' => array(
+        array('group' => 'order', 'name' => 'jkxRecalculateOrderArticlePrice', 'type' => 'bool', 'value' => 'false')
+    )
+);
+
